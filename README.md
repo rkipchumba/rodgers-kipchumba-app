1feab6c4fb1ba2dba71a479cf00aec64985359d8
rkipchumba <kipchumbarodgers@gmail.com>
2025-11-12 17:41:24 +0300

    docs update

--- README.md
+++ README.md
@@ -15,3 +15,10 @@ It connects to a mock REST API (`https://69147c603746c71fe0487070.mockapi.io/api
 ```bash
 git clone https://github.com/rkipchumba/KcbFrontEnd.git
 cd KcbTask
+```
+
+## Install dependencies:
+- npm install
+
+## Run development server:
+- npm run dev
--- src/api/index.ts
+++ src/api/index.ts
@@ -8,20 +8,4 @@ const api = axios.create({
   },
 });
 
-api.interceptors.request.use(config => {
-  const token = localStorage.getItem("token");
-  if (token && config.headers) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
-});
-
-api.interceptors.response.use(
-  response => response,
-  error => {
-    console.error("API Error:", error);
-    return Promise.reject(error);
-  }
-);
-
 export default api;
